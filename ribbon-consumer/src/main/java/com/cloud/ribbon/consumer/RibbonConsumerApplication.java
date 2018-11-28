48535a98e8a89ee58113324a42bd64c05b94ca3d
sunfang555 <525639648@qq.com>
2018-11-28 16:47:57 +0800

    xiangmu

---
+++ ribbon-consumer/src/main/java/com/cloud/ribbon/consumer/RibbonConsumerApplication.java
@@ -0,0 +1,28 @@
+package com.cloud.ribbon.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@ComponentScan
+@EnableDiscoveryClient
+public class RibbonConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonConsumerApplication.class, args);
+	}
+
+	@Bean
+	@Autowired
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+}
---
+++ ribbon/src/main/java/com/cloud/ribbon/RibbonApplication.java
@@ -0,0 +1,26 @@
+package com.cloud.ribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@ComponentScan
+@EnableDiscoveryClient//注册服务
+public class RibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonApplication.class, args);
+	}
+	@Bean
+	@Autowired
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
